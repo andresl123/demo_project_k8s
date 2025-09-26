54e66ed7af8740ec65b5093ceddebd9d46218e5b
andresl123 <andreslopes2009@gmail.com>
2025-09-26 14:12:39 -0400

    Update HelloController.java
    test CI/CD

--- src/main/java/com/helloword/demo/controller/HelloController.java
+++ src/main/java/com/helloword/demo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/api/hello")
   public String hello() {
-    return "Hello, World from Spring Boot!";
+    return "Hello, World from Spring Boot! test";
   }
 }
